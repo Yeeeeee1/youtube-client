dd46219249927acac89bd5d9ca2d47b9d21e7294
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-07-29 20:57:35 +0300

    feat: add components and interfaces

---
+++ my-dream-app/src/app/models/search-result.model.ts
@@ -0,0 +1,45 @@
+export interface IResult {
+  kind: string;
+  etag: string;
+  pageInfo: { [key: string]: number };
+  items: ISearchItems[];
+}
+
+interface ISearchItems {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: ISnippet;
+  statistics: { [key: string]: string };
+}
+
+interface ISnippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: {
+    default: IThumbailsItem;
+    medium: IThumbailsItem;
+    high: IThumbailsItem;
+    standard: IThumbailsItem;
+    maxres: IThumbailsItem;
+  };
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: localized;
+  defaultAudioLanguage: string;
+}
+
+interface IThumbailsItem {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface localized {
+  title: string;
+  description: string;
+}
