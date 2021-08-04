dc120099505c1af9d16dfcdc877e6bf752e300ec
Konstantin <KostyaEK2006@gmail.com>
2021-08-04 21:14:07 +0300

    feat: add some functional for directive

--- youtube-client/src/app/search/search-results/directives/highlight-bottom.directive.ts
+++ youtube-client/src/app/search/search-results/directives/highlight-bottom.directive.ts
@@ -1,8 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightBottom]',
 })
 export class HighlightBottomDirective {
-  constructor() {}
+  @Input() set appHighlightBottom(publishedAt: string) {
+    let color;
+    if (publishedAt) {
+      let publishDate: any = new Date(publishedAt);
+      let nowDate: any = new Date();
+      let diff = nowDate - publishDate;
+      if (diff < 30) {
+        color = 'green';
+      }
+      if (publishDate < 7) {
+        color = 'blue';
+      }
+      if (publishDate > 180) {
+        color = 'red';
+      }
+      console.log(diff / 1000 / 60 / 60 / 24);
+    }
+    this.el.nativeElement.style.borderBottom = ' 2px solid ' + color;
+  }
+
+  constructor(private el: ElementRef) {}
 }
