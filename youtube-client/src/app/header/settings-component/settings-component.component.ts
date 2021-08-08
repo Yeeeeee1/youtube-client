fcbdb8b40015bb6ac5ec3e9dfac6e9209a429d58
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2021-08-08 19:09:26 +0300

    Revert "Angular intro"

--- youtube-client/src/app/header/settings-component/settings-component.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-settings-component',
-  templateUrl: './settings-component.component.html',
-  styleUrls: ['./settings-component.component.scss'],
-})
-export class SettingsComponentComponent {}
